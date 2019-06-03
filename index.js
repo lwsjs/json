fb5735a12c284c0fa12224643e4bf01bf6207d97
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-06-03 13:25:15 +0100

    upgrade lws

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-module.exports = MiddlewareBase => class Json extends MiddlewareBase {
+class Json {
   description () {
     return 'Pretty-prints JSON responses. Also converts node object streams to binary.'
   }
+
   middleware () {
     const json = require('koa-json')
     return json()
   }
 }
+
+module.exports = Json
--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-const TestRunner = require('test-runner')
+const Tom = require('test-runner').Tom
 const Json = require('./')
 const Lws = require('lws')
-const request = require('req-then')
+const fetch = require('node-fetch')
 
-const runner = new TestRunner()
+const tom = module.exports = new Tom('json')
 
-runner.test('simple', async function () {
+tom.test('simple', async function () {
   const port = 8000 + this.index
-  const lws = new Lws()
-  const server = lws.create({ port, stack: Json })
-  const response = await request(`http://localhost:${port}/`)
-  server.close()
+  const lws = Lws.create({ port, stack: Json })
+  const response = await fetch(`http://localhost:${port}/`)
+  lws.server.close()
 })
